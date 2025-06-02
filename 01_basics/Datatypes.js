11f3e021b223e63005327bb10224e175f492bd04
AnshulRaturi17 <anshulraturi08@gmail.com>
2025-06-03 00:32:50 +0530

    Memory location of Datatypes

--- 01_basics/Datatypes.js
+++ 01_basics/Datatypes.js
@@ -20,4 +20,25 @@ console.log(typeof obj) //object
 let fun = function() { console.log("hello world"); }
 console.log(typeof fun) //function
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Two types of memory location 
+// Stack (Primitive Datatypes are stored) -> a copy is made pass by value
+// Heap (Non-primitive are stored) -> original memory is shared , pass by refrence
+
+let Rollno20 = "Anshul"
+let Rollno21 = Rollno20 // a copy is given to Rollno21 
+Rollno21 = "Shubhanshi"
+console.log(Rollno20);
+console.log(Rollno21);
+
+let user1 = {
+    email:"anshul@google.com",
+    id:22121
+}
+let user2 = user1;
+user2.email = "ratti@jnv.com" // both user1 and user2 point to same location
+console.log(user1.email);
+console.log(user2.email)
+
 
