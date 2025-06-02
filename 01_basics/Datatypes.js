336898fbf278e7ad9b6dc29ade093b91d0fb7ad0
AnshulRaturi17 <anshulraturi08@gmail.com>
2025-06-03 00:19:18 +0530

    adding more inforamation of typeof datatypes

--- 01_basics/Datatypes.js
+++ 01_basics/Datatypes.js
@@ -2,6 +2,22 @@
 // primitive
 /**
  Number,String,Boolean,Bigint,null,Undefined
- */
+  */
 let a = 10;
-console.log(a)
+// console.log(a)
+let name = "Anshul"
+// console.log(name);
+// console.log(typeof name)
+ 
+//2nd type => Non Primitive ya Refrence
+// array , object , function
+let arr = ["anshul","is","good","boy"]
+console.log(arr)
+console.log(typeof arr) // object
+let obj = {1:2,2:3}
+console.log(obj)
+console.log(typeof obj) //object
+let fun = function() { console.log("hello world"); }
+console.log(typeof fun) //function
+
+
