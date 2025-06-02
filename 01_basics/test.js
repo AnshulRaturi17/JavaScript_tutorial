81f2aef269edee2c63941509e8a5d5c16762c33f
AnshulRaturi17 <anshulraturi08@gmail.com>
2025-06-02 11:05:22 +0000

    variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
-console.log("anshul")
+const accoundId = 220112032
+let name = "Anshul"
+var password = "1234"
+// accoundId = 2218408
+console.log(accoundId);
+password = "2222"
+accountCity = "dun"
+let accountState;//it is undefined in js
+console.table([accoundId,name,password,accountCity])
+/*
+prefer to use let rather than var because of block scope and
+functional scope
+*/
---
+++ 01_basics/variables.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all code to newer version
+//alert(3 + 3); // not work because we are using nodejs not browser
+
+//number => 2 - 2^53
+//bigint
+//string = ""
+//boolean = true/false
+//null => standalone value
+//undefined => abhi uske ander koi value nahi hai
+//symbol => unique , used in react and all
+
+/*these all are primitive datatypes */
+
+//object
+let name = "anshul"
+let age = 22
+console.log(typeof null); // null -> type =? object
+console.log(typeof undefined); // undefined
+
+
